cef57fe3023af084646744ff2b71e761295924ee
Dharshini <dharshinisatha54@gmail.com>
2026-03-19 20:56:51 +0530

    fix: allow Netlify origin in CORS

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: [
+    'http://localhost:5173',
+    'https://pencraftblog.netlify.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
